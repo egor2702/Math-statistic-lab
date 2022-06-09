8a88cb51e571a42447454a7df841f4e44d001080
Egor <2003egor2702@gmail.com>
2022-06-09 09:39:36 +0300

    Add sample's visualisation

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import numpy as np
+from matplotlib import pyplot as plt
 
+x_fact = np.array([8, 7.8, 7.6, 7.7, 7.7, 7, 7.3, 7.1, 6.6])
+y = np.array([16.8, 16.7, 16.7, 16.6, 16.6, 16.6, 16.6, 16.6, 16.4])
+
+fig = plt.figure()
+fig.suptitle("Sample's visualisation")
+ax = fig.add_subplot()
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.scatter(x_fact, y)
+ax.grid()
+plt.show()
